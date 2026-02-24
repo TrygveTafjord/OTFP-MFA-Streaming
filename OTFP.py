90c8bc683f16427ffefa744196a72a1350c3f672
Trygve Myrland Tafjord <trygvemt@ntnu.no>
2026-02-24 09:54:40 +0100

    do

--- OTFP.py
+++ OTFP.py
@@ -15,7 +15,7 @@ class MFA_OTFP:
         data_dir = glob.glob(f'data/training_l1b/*.nc')
         initil_data = fetch_init_data(dir = data_dir, num_samples = 20000) #Not yet implemented, just a placeholder for fetching initial data for model selection and initial fitting, will probably be implemented as a method in this class later on, or perhaps even as a separate utility-function or perhaps the data will be an input to the constructor, we will see.
         K, q = self.perform_model_selection(data=initil_data, n_channels=n_channels, q_max=q_max)
-        
+
         self.MFA = MFA(n_components = K, n_channels = n_channels, n_factors=q).to(device)        
         self.MFA.fit(initil_data) # Fit the initial model on the initial data
         
@@ -23,13 +23,14 @@ class MFA_OTFP:
 
         # Outliers are saved to the shelf
         self.outliers_shelf = torch.empty((self.outlier_update_treshold, n_channels), device=self.device, dtype=torch.float32)
+        self.pixels_assigned_per_component = torch.empty((self.outlier_update_treshold, n_channels), device=self.device, dtype=torch.float32)
+        
         # System State
         self.n_samples_seen = 0
         self.num_outliers_on_shelf = 0
         self.current_idx_in_chunk = 0
         self.batch_file_counter = 0
         self.n_model_updates = 0
-
     
     def perform_model_selection(self, data, n_channels, q_max):
         # Dummy implementation
@@ -43,36 +44,17 @@ class MFA_OTFP:
         log_resp_norm, log_likelihood = self.MFA.e_step(X)
 
         
+
+
         # Update counter
         self.n_samples_seen += X.shape[0]
 
+        return log_resp_norm, log_likelihood
 
-
-    def save_current_chunk(self):
-        """Saves the valid data in the current buffers to a file."""
-        if self.current_idx_in_chunk == 0:
-            return # Nothing to save
-
-        # Get views of the valid data 
-        valid_T = self.T_buffer[:self.current_idx_in_chunk]
-        valid_res = self.res_buffer[:self.current_idx_in_chunk]
-        valid_T2 = self.T2_buffer[:self.current_idx_in_chunk]
-        valid_F_res = self.F_res_buffer[:self.current_idx_in_chunk]
-        valid_Q_res = self.Q_buffer[:self.current_idx_in_chunk]
-        
-        dir = "./data/processed/"
-        filename = f"CIM_batch_full_{self.batch_file_counter}.pt"
-        #print(f"Saving {self.current_idx_in_chunk} samples to {filename}")
-        save_loaction = dir + filename
-
-        # This a blocking I/O operation.
-        torch.save({
-            'T': valid_T.cpu(), # Move to CPU for saving
-            'Residuals': valid_res.cpu(),
-            'T2': valid_T2.cpu(),
-            'F_res': valid_F_res.cpu(),
-            'Q_res': valid_Q_res.cpu(), 
-        }, save_loaction)
+    def fetch_init_data(self, dir, num_samples):
+        # Dummy implementation
+        return torch.randn(num_samples, self.n_channels, device=self.device)
+    
 
 
 
